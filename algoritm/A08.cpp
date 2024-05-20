833ff8d0fdeea24df4552e08ea061edd331d2e29
satokenn <odinn3103@gmail.com>
2024-05-21 00:48:48 +0900

    コードにコメントを書き足した

--- algoritm/A08.cpp
+++ algoritm/A08.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main() {
     int h, w, q;
     cin >> h >> w;
-    vector<vector<int>> x(h + 1, vector<int>(w + 1, 0)), r(h + 1, vector<int>(w + 1, 0));
+    vector<vector<int>> x(h + 1, vector<int>(w + 1, 0)), r(h + 1, vector<int>(w + 1, 0)); // 二次元配列rは累積和用.
+
 
     for(int i = 1; i <= h; i++) {
         for(int j = 1; j <= w; j++) {
@@ -13,6 +14,7 @@ int main() {
         }
     }
 
+    // 横方向(x方向)に累積和をとる
     for(int i = 1; i <= h; i++) {
         r[i][1] = x[i][1];
         for(int j = 2; j <= w; j++) {
@@ -20,6 +22,7 @@ int main() {
         }
     }
 
+    // 縦方向(y方向)に累積和をとる
     for(int i = 1; i <= w; i++) {
         for(int j = 2; j <= h; j++){
             r[j][i] += r[j - 1][i];
